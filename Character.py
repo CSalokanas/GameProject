7d6eab3d5d0d9f96a2bca0cdaac5c4073457ce4c
Christos <christossalokanas@gmail.com>
2025-02-27 22:10:53 +0100

    adding left and right movement functions in the character class

--- Character.py
+++ Character.py
@@ -4,8 +4,21 @@ class Aidn5(pygame.sprite.Sprite):
 
     def __init__(self, x):
         self.image = pygame.transform.scale2x(pygame.image.load(character_image)).convert_alpha()
-        self.player_pos = (x, 780)
+        self.x = 512
+        self.player_pos = (self.x, 780)
+        self.character = pygame.image.load(character_image)
+        self.speed = 5
         super().__init__()
 
-    def start(self, screen, player_pos):
-        pygame.draw.circle(screen, "red", player_pos, 40)
+    def start(self, screen):
+        pygame.draw.circle(screen, "red", self.player_pos, 40)
+
+
+    def move_left(self):
+        self.x += self.speed
+        pygame.display.update()
+
+
+    def move_right(self):
+        self.x -= self.speed
+        pygame.display.update()
