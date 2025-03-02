b62d5346c2362bac84f7ee86d652033ff7aa76df
Christos <christossalokanas@gmail.com>
2025-03-02 21:36:20 +0100

    Button class

--- Buttons.py
+++ Buttons.py
@@ -1,18 +1,51 @@
 import pygame
 
-
-
 class Buttons:
-    def __init__(self, text,x_pos,y_pos,enabled, screen):
-        self.font = pygame.font.Font("freesansbold.ttf", 20)
-        self.text = text
-        self.text = self.font.render(text, True, (255, 255, 255))
-        self.x = x_pos
-        self.y = y_pos
-        self.enabled = enabled
-        self.screen = screen
+    def __init__(self, text, screen, size_x=150, size_y=50, font="freesansbold.ttf", text_size=20,button_color ="gray",x_pos=0,y_pos=0, text_color="white", text_shadow = False, aliases = True, borders = False, line_thickness = 2, border_color = "red"):
+        self.button_map = {
+            "Size_X": size_x,
+            "Size_Y": size_y,
+            "Color": button_color,
+            "Text": text,
+            "Text_Size": text_size,
+            "Text_Color": text_color,
+            "Position_X": x_pos,
+            "Position_Y": y_pos,
+            "Font": font,
+            "Screen": screen,
+            "Text_Shadow": text_shadow,
+            "aliases": aliases,
+            "Borders": borders,
+            "Line_Thickness": line_thickness,
+            "Border_Color": border_color
+        }
+        self.font = pygame.font.Font(font, text_size)
+        self.text = self.font.render(text, self.button_map["aliases"], text_color)
+        self.shadow_text = self.font.render(text, self.button_map["aliases"], "black")
+    def draw(self):
+        button_rect = pygame.rect.Rect(self.button_map["Position_X"], self.button_map["Position_Y"], self.button_map["Size_X"], self.button_map["Size_Y"])
+        pygame.draw.rect(surface=self.button_map["Screen"], color=self.button_map["Color"], rect=button_rect)
+        self.button_map["Screen"].blit(self.text, (self.button_map["Position_X"] + 10, self.button_map["Position_Y"] + 10))
+        if self.button_map["Borders"]:
+            pygame.draw.line(surface=self.button_map["Screen"], color=self.button_map["Border_Color"], start_pos=(self.button_map["Position_X"], self.button_map["Position_Y"]), end_pos=(self.button_map["Position_X"] + self.button_map["Size_X"], self.button_map["Position_Y"]), width=self.button_map["Line_Thickness"])
+            pygame.draw.line(surface=self.button_map["Screen"], color=self.button_map["Border_Color"], start_pos=(self.button_map["Position_X"], self.button_map["Position_Y"]), end_pos=(self.button_map["Position_X"], self.button_map["Position_Y"] + self.button_map["Size_Y"]), width=self.button_map["Line_Thickness"])
+            pygame.draw.line(surface=self.button_map["Screen"], color=self.button_map["Border_Color"],start_pos=(self.button_map["Position_X"], self.button_map["Position_Y"]+self.button_map["Size_Y"]), end_pos=(self.button_map["Position_X"] + self.button_map["Size_X"], self.button_map["Position_Y"]+self.button_map["Size_Y"]), width=self.button_map["Line_Thickness"])
+            pygame.draw.line(surface=self.button_map["Screen"], color=self.button_map["Border_Color"],start_pos=(self.button_map["Position_X"] + self.button_map["Size_X"], self.button_map["Position_Y"]), end_pos=(self.button_map["Position_X"] + self.button_map["Size_X"], self.button_map["Position_Y"] + self.button_map["Size_Y"]), width=self.button_map["Line_Thickness"])
+        if self.button_map["Text_Shadow"]:
 
-    def draw(self,):
-        button_rect = pygame.rect.Rect(self.x, self.y, 100, 50)
-        pygame.draw.rect(surface=self.screen, color="gray", rect=button_rect)
-        self.screen.blit(self.text, (self.x + 10, self.y + 10))
+            self.button_map["Screen"].blit(self.shadow_text,
+                                           (self.button_map["Position_X"] + 12, self.button_map["Position_Y"] + 12))
+            self.button_map["Screen"].blit(self.text,
+                                           (self.button_map["Position_X"] + 10, self.button_map["Position_Y"] + 10))
+        else:
+            self.button_map["Screen"].blit(self.text,
+                                           (self.button_map["Position_X"] + 10, self.button_map["Position_Y"] + 10))
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if pygame.mouse.get_just_pressed()[0]:
+            if self.button_map["Position_X"] < mouse_pos[0] < self.button_map["Size_X"] + self.button_map["Position_X"]:
+                if self.button_map["Position_Y"] < mouse_pos[1] < self.button_map["Size_Y"] + self.button_map["Position_Y"]:
+                    return True
+        return False
+    def text_shadow(self):
+        self.text = self.font.render(self.button_map["Text"], self.button_map["aliases"], "black")
